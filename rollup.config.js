f8952786e93b37ebf4f55cb6d0fe8aa260fbc903
Tomas Lakauskas <thfrlw@icloud.com>
2017-09-21 10:59:02 +0300

    clean up

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
-import json from 'rollup-plugin-json';
 
 export default {
 	input: './module',
@@ -42,25 +41,13 @@ export default {
 		nodeResolve ({
 			jsnext: true,
 			main: true,
+			module: true,
 			preferBuiltins: true
 		}),
 		commonjs ({
 			include: 'node_modules/**',
-			ignore: ['emitter', 'socket.io-client/package'],
 			ignoreGlobal: false,
 			sourceMap: false
-		}),
-		json()
+		})
 	]
 };
-
-
-/* TODO:
-	1:
-	remove in files:
-		node_modules/browser-sync/index.js
-		node_modules/dev-ip/lib/dev-ip.js
-
-	comment 124 - 136 lines in file: node_modules/aws-sign2/index.js
-	comment 9 - 11, 13 lines in file: node_modules/ws/lib/Bufferutil.js
-*/
